3011b42f63b164a342baea585a657aa83dfbfc64
DanielGomesOtt <danielgomesott@gmail.com>
2026-05-17 01:54:30 -0300

    feat(incident scheduler): add logging for incident scheduler

--- src/main/java/com/sentinel/sentinel/schedulers/IncidentScheduler.java
+++ src/main/java/com/sentinel/sentinel/schedulers/IncidentScheduler.java
@@ -1,12 +1,15 @@
 package com.sentinel.sentinel.schedulers;
 
 import com.sentinel.sentinel.services.IncidentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IncidentScheduler {
 
+    private static final Logger log = LoggerFactory.getLogger(IncidentScheduler.class);
     private final IncidentService incidentService;
 
     public IncidentScheduler(IncidentService incidentService) {
@@ -18,7 +21,7 @@ public class IncidentScheduler {
         try {
             incidentService.verifyExpiredSla();
         } catch (Exception exception) {
-            System.out.println(exception.getMessage());
+            log.error(exception.getMessage());
         }
     }
 }
