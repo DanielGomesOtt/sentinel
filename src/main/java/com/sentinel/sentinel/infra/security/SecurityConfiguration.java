c33660d43c43f810e9ea8232c1215101a6eb172c
DanielGomesOtt <danielgomesott@gmail.com>
2026-05-20 22:57:08 -0300

    chore: remove unused imports

--- src/main/java/com/sentinel/sentinel/infra/security/SecurityConfiguration.java
+++ src/main/java/com/sentinel/sentinel/infra/security/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package com.sentinel.sentinel.infra.security;
 
-import com.sentinel.sentinel.enums.Roles;
 import com.sentinel.sentinel.utils.ApiVersionUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
 import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
@@ -22,8 +20,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.HttpStatusAccessDeniedHandler;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
--- src/main/java/com/sentinel/sentinel/infra/security/SecurityFilter.java
+++ src/main/java/com/sentinel/sentinel/infra/security/SecurityFilter.java
@@ -1,6 +1,5 @@
 package com.sentinel.sentinel.infra.security;
 
-import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.sentinel.sentinel.models.AuthenticatedPrincipal;
 import com.sentinel.sentinel.models.SystemIntegration;
@@ -17,7 +16,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
