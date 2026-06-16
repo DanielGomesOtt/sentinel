f13b12a745c844475ab41ab5f23fa6be5d30359b
DanielGomesOtt <danielgomesott@gmail.com>
2026-06-15 22:54:27 -0300

    test(auth service): complete AuthService test coverage

--- src/test/java/com/sentinel/sentinel/services/AuthServiceTest.java
+++ src/test/java/com/sentinel/sentinel/services/AuthServiceTest.java
@@ -2,12 +2,16 @@ package com.sentinel.sentinel.services;
 
 import com.sentinel.sentinel.dto.auth.AuthenticatedUserDTO;
 import com.sentinel.sentinel.dto.auth.RegisterUserDTO;
+import com.sentinel.sentinel.dto.system_integration.ClientAuthDTO;
+import com.sentinel.sentinel.dto.system_integration.TokenResponseDTO;
 import com.sentinel.sentinel.enums.Roles;
 import com.sentinel.sentinel.exceptions.UserAlreadyExistException;
 import com.sentinel.sentinel.infra.security.SecurityConfiguration;
 import com.sentinel.sentinel.models.Organization;
+import com.sentinel.sentinel.models.SystemIntegration;
 import com.sentinel.sentinel.models.Users;
 import com.sentinel.sentinel.repositories.OrganizationRepository;
+import com.sentinel.sentinel.repositories.SystemIntegrationRepository;
 import com.sentinel.sentinel.repositories.UsersRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -18,10 +22,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,6 +38,9 @@ class AuthServiceTest {
 
     @Mock
     private OrganizationRepository organizationRepository;
+
+    @Mock
+    private SystemIntegrationRepository systemIntegrationRepository;
 
     @Mock
     private TokenService tokenService;
@@ -138,6 +147,93 @@ class AuthServiceTest {
         assertThrows(UserAlreadyExistException.class, () -> {
             authService.register(dto);
         });
+    }
+
+    @Test
+    @DisplayName("generate token should generate and return a token")
+    void generateTokenShouldReturnToken() {
+        ClientAuthDTO data = new ClientAuthDTO("client_id", "client_secret");
+        SystemIntegration systemIntegration = new SystemIntegration();
+        String hashedSecret = BCrypt.hashpw(data.clientSecret(), BCrypt.gensalt());
+        systemIntegration.setClientSecretHash(hashedSecret);
+        systemIntegration.setActive(true);
+        String token = "access token";
+
+        when(systemIntegrationRepository.findByClientId(data.clientId()))
+                .thenReturn(Optional.of(systemIntegration));
+
+        when(tokenService.signSystemToken(systemIntegration)).thenReturn(token);
+
+        TokenResponseDTO result = authService.generateToken(data);
+
+        assertNotNull(result);
+        assertEquals("access token", token);
+
+        verify(tokenService).signSystemToken(systemIntegration);
+    }
+
+    @Test
+    @DisplayName("generate token should throw UsernameNotFoundException")
+    void generateTokenShouldThrowUsernameNotFoundException() {
+        ClientAuthDTO data = new ClientAuthDTO("client_id", "client_secret");
+        SystemIntegration systemIntegration = new SystemIntegration();
+        String hashedSecret = BCrypt.hashpw(data.clientSecret(), BCrypt.gensalt());
+        systemIntegration.setClientSecretHash(hashedSecret);
+        systemIntegration.setActive(true);
+
+        when(systemIntegrationRepository.findByClientId(data.clientId()))
+                .thenReturn(Optional.empty());
+
+        assertThrows(UsernameNotFoundException.class, () -> {
+           authService.generateToken(data);
+        }, "Could not complete the authentication with the provided information.");
+    }
+
+    @Test
+    @DisplayName("generate token should throw UsernameNotFoundException when client secret is invalid")
+    void generateTokenShouldThrowUsernameNotFoundExceptionWhenClientSecretInvalid() {
+        ClientAuthDTO data = new ClientAuthDTO("client_id", "client_secret");
+        SystemIntegration systemIntegration = new SystemIntegration();
+        String hashedSecret = "invalid hashed secret";
+        systemIntegration.setClientSecretHash(hashedSecret);
+        systemIntegration.setActive(true);
+        String token = "access token";
+
+        when(systemIntegrationRepository.findByClientId(data.clientId()))
+                .thenReturn(Optional.of(systemIntegration));
+
+
+        assertThrows(Exception.class, () -> {
+            authService.generateToken(data);
+        }, "Could not complete the authentication with the provided information.");
+
+    }
+
+    @Test
+    @DisplayName("generate token should throw UsernameNotFoundException when client is inactive")
+    void generateTokenShouldThrowUsernameNotFoundExceptionWhenClientIsInactive() {
+
+        ClientAuthDTO data = new ClientAuthDTO(
+                "client_id",
+                "client_secret"
+        );
+
+        String hashedSecret = BCrypt.hashpw(
+                data.clientSecret(),
+                BCrypt.gensalt()
+        );
+
+        SystemIntegration systemIntegration = new SystemIntegration();
+        systemIntegration.setClientSecretHash(hashedSecret);
+        systemIntegration.setActive(false);
+
+        when(systemIntegrationRepository.findByClientId(data.clientId()))
+                .thenReturn(Optional.of(systemIntegration));
+
+        assertThrows(
+                UsernameNotFoundException.class,
+                () -> authService.generateToken(data)
+        );
     }
 
 }
