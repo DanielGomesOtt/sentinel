e8e60403f21aa61b9df9a18e1eabaeef10e39865
DanielGomesOtt <danielgomesott@gmail.com>
2026-07-07 23:24:52 -0300

    chore: add SMTP mail environment variable

--- src/main/java/com/sentinel/sentinel/services/EmailService.java
+++ src/main/java/com/sentinel/sentinel/services/EmailService.java
@@ -1,5 +1,6 @@
 package com.sentinel.sentinel.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${smtp.mail}")
+    private String email;
+
 
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
@@ -17,6 +21,7 @@ public class EmailService {
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
 
+        message.setFrom(this.email);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
